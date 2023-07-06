57dc2d2e66cfca143134e39402cf7bec82d4cd10
Halliwood <linxiaojia8802@126.com>
2023-07-06 19:55:02 +0800

    change json EOL to crlf

--- src/maintain.ts
+++ src/maintain.ts
@@ -7,9 +7,9 @@ import UnitySoftTasks from "./example/UnitySoftTasks";
 async function makeTaskJson(): Promise<void> {
     const jsonRoot = path.join(__dirname, 'builtinTasks');
     await fs.ensureDir(jsonRoot);
-    await fs.writeJSON(path.join(jsonRoot, 'unity_hard.json'), UnityHardTasks, { spaces: 4});
-    await fs.writeJSON(path.join(jsonRoot, 'unity_soft.json'), UnitySoftTasks, { spaces: 4});
-    await fs.writeJSON(path.join(jsonRoot, 'laya_hard.json'), LayaTasks, { spaces: 4});
+    await fs.writeJSON(path.join(jsonRoot, 'unity_hard.json'), UnityHardTasks, { spaces: 4, EOL: '\r\n' });
+    await fs.writeJSON(path.join(jsonRoot, 'unity_soft.json'), UnitySoftTasks, { spaces: 4, EOL: '\r\n' });
+    await fs.writeJSON(path.join(jsonRoot, 'laya_hard.json'), LayaTasks, { spaces: 4, EOL: '\r\n' });
 }
 
 makeTaskJson();
